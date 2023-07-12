5667aecc90264dff49c3c22e217ee9c58bad819a
Joe Schoech <j.schoech@gmail.com>
2023-07-12 06:46:08 -0400

    delete css

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <h3>Next 13 Firebase Auth Example</h3>
+        <h3>Next 13 React Server Components Firebase Auth Example</h3>
         <h5>As of june 11 2023</h5>
         <h5>
           <Link href={"https://github.com/joeschoe/nextfire"}>
